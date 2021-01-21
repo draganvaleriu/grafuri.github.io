97b7cf39082e2c8d2e88a7051e428a43c3790308
valeriudragan <draganvaleriu8@gmail.com>
2021-01-21 21:24:21 +0200

    edit1

--- pages/javascript.js
+++ pages/javascript.js
@@ -1,62 +1,39 @@
 window.onscroll = function(){
  	var pos = document.documentElement.scrollTop;
- 	if(pos > 10 && pos < 1800)
+ 	let p1 =document.getElementById("unu").getBoundingClientRect().top + pos;
+ 	let p2 = document.getElementById('doi').getBoundingClientRect().top + pos ;
+ 	let p3 = document.getElementById('trei').getBoundingClientRect().top + pos;
+ 	let p4 = document.getElementById('patru').getBoundingClientRect().top + pos;
+ 	let p5 = document.getElementById('cinci').getBoundingClientRect().top + pos;
+ 	let p6 = document.getElementById('sase').getBoundingClientRect().top + pos; 
+ 	if(pos > 0 && pos < p2-200){
  		document.getElementById("term").className = "AAA";
- 	else
- 		document.getElementById("term").className = "";
- 	if(pos > 1800 && pos < 2500)
- 		document.getElementById("grad").className = "AAA";
- 	else
- 		document.getElementById("grad").className = "";
- 	if(pos > 2600 && pos < 6000)
- 		document.getElementById("reperez").className = "AAA";
- 	else
- 		document.getElementById("reperez").className = "";
- 	if(pos > 3200 && pos < 4000)
- 		document.getElementById("Matricea").className = "AAA";
- 	else
- 		document.getElementById("Matricea").className = "";
- 
- 	if(pos > 4100 && pos < 5000)
- 		document.getElementById("muchii").className = "AAA";
- 	else
- 		document.getElementById("muchii").className = "";
- 	if(pos > 5100 && pos < 6000)
- 		document.getElementById("vecini").className = "AAA";
- 	else
- 		document.getElementById("vecini").className = "";
- 	if(pos >6100 && pos < 7000)
- 		document.getElementById("partial").className = "AAA";
- 	else
- 		document.getElementById("partial").className = "";
- 	if(pos > 7100 && pos < 8000)
- 		document.getElementById("nul").className = "AAA";
- 	else
- 		document.getElementById("nul").className = "";
- 	if(pos > 8100 && pos < 9000)
- 		document.getElementById("conex1").className = "AAA";
- 	else
- 		document.getElementById("conex1").cconex1lassName = "";
-	if(pos > 8100 && pos < 9000)
- 		document.getElementById("lant").className = "AAA";
- 	else
- 		document.getElementById("lant").cconex1lassName = "";
- 	if(pos > 8100 && pos < 9000)
- 		document.getElementById("conex2").className = "AAA";
- 	else
- 		document.getElementById("conex2").cconex1lassName = "";
- 	if(pos > 8100 && pos < 9000)
- 		document.getElementById("pad").className = "AAA";
- 	else
- 		document.getElementById("pad").cconex1lassName = "";
- 	if(pos > 8100 && pos < 9000)
- 		document.getElementById("ham").className = "AAA";
- 	else
- 		document.getElementById("ham").cconex1lassName = "";	
+ 	 }
+ 	 else{
+ 	 	document.getElementById("term").className = "";
+ 	 }
+ 	  if(pos >= p2- 200 && pos < p3){
+ 	 	document.getElementById("grad").className = "AAA";
+ 	 }
+ 	 else{
+ 	 	document.getElementById("grad").className = "";
+ 	 }
+ 	 if(pos >= p3- 200 && pos < p4){
+ 	 	document.getElementById("reprez").className = "AAA";
+ 	 }
+ 	 else{
+ 	 	document.getElementById("reprez").className = "";
+ 	 }
+  
+ 	 
  };
  
-  
-function q(pix)
+ function a(elem) {
+  let box = elem.getBoundingClientRect();
+ 
+    b(box.top + pageYOffset);  
+}
+function b(pix)
 { 
 	window.scroll(0,pix);	 
 }
